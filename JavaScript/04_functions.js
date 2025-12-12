19e4e3fcbcaf9d6ff158b27f543a7ffa87dd77dd
hopesain <hopessain@gmail.com>
2025-12-12 07:33:14 +0200

    functions done

---
+++ JavaScript/04_functions.js
@@ -0,0 +1,15 @@
+function celciusToKelvins(celcius) {
+    return celcius + 273
+}
+
+function kelvinToCelcius(kelvin) {
+    return kelvin - 273
+}
+
+let value = celciusToKelvins(18)
+console.log(value + " Kelvins")
+
+
+const add = (a, b) => a + b
+
+console.log(add(2, 3))
