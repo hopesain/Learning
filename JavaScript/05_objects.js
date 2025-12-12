0cc7221ec448b554a84cb0918a68c6308889041d
hopesain <hopessain@gmail.com>
2025-12-12 08:01:26 +0200

    intro to objects

---
+++ JavaScript/05_objects.js
@@ -0,0 +1,24 @@
+const person = {
+    firstName: "Hope",
+    lastName: "Sain",
+    age: 24,
+    fullName: function() {
+        return this.firstName + " " + this.lastName
+    }
+}
+
+console.log(person)
+console.log(person.fullName())
+
+function User(firstName, lastName, age, email) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.age = age,
+    this.email = email
+}
+
+const firstUser = new User("Hope", "Sain", 24, "hopesain@email.com")
+const secondUser = new User("Trevor", "Makwinja", 25, "tre@email.com")
+
+console.log(firstUser)
+console.log(secondUser)
