1ae606e85587f284da26ae5bb4dacda867fea891
hopesain <hopessain@gmail.com>
2025-12-30 18:56:19 +0200

    template literals.

---
+++ JavaScript/11_template_literals.js
@@ -0,0 +1,12 @@
+// Template Literals 
+
+console.log("Template Literals.")
+
+let _name = "Hope Sain"
+let greeting = `Hello ${_name}`
+console.log(greeting)
+
+let firstNumber = 10
+let secondNumber = 5
+let sum = `The sum of ${firstNumber} and ${secondNumber} is ${firstNumber + secondNumber}.`
+console.log(sum)
