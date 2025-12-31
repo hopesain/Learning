b4677d62609706d2a8c00c9cc6f36608de3610be
hopesain <hopessain@gmail.com>
2025-12-31 08:59:49 +0200

    completed inheritance.

--- JavaScript/12_classes.js
+++ JavaScript/12_classes.js
@@ -1,7 +1,7 @@
 // Classes
 console.log("Classes.")
 
-class Person {
+class User {
     constructor(username, age) {
         this.username = username
         this.age = age
@@ -13,8 +13,25 @@ class Person {
     }
 }
 
-let firstPerson = new Person("hopesain", 24)
-let secondPerson = new Person("trevor", 25)
+let firstUser = new User("hopesain", 24)
+let secondUser = new User("trevor", 25)
 
-console.log(firstPerson.greet())
-console.log(secondPerson.greet())
+console.log(firstUser.greet())
+console.log(secondUser.greet())
+
+// Inheritance 
+
+class Student extends User {
+    constructor(username, age, program) {
+        super(username, age)
+        this.program = program
+
+    }
+
+    getStudentInformation() {
+        return `Student Name: ${this.username}, Student Age: ${this.age}, Program of Study: ${this.program}` 
+    }
+}
+
+let firstStudent = new Student("Hope Sain", 24, "Agricultural Innovations")
+console.log(firstStudent.getStudentInformation())
