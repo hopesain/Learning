49361fa1efe1f560bb886fc650c7989d88ba4da4
hopesain <hopessain@gmail.com>
2026-01-25 08:44:33 +0200

    arrow functions.

--- TypeScript/dist/01_functions.js
+++ TypeScript/dist/01_functions.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let first_number = 5_000;
+let second_number = 10_000;
 function check_availability(income) {
     if (income < 10_000) {
         return true;
@@ -12,12 +14,17 @@ console.log(check_availability(income));
 function add(first_number, second_number) {
     return first_number + second_number;
 }
-let first_number = 5_000;
-let second_number = 10_000;
 console.log(add(first_number, second_number));
 //Anonymous Functions
 let multipy = function (first_number, second_number) {
     return first_number * second_number;
 };
 console.log(multipy(first_number, second_number));
+//Arrow functions
+let first_name = "Hope";
+let last_name = "Sain";
+let full_name = (first_name, last_name) => {
+    return first_name + " " + last_name;
+};
+console.log(full_name(first_name, last_name));
 //# sourceMappingURL=01_functions.js.map
--- TypeScript/src/01_functions.ts
+++ TypeScript/src/01_functions.ts
@@ -1,3 +1,6 @@
+let first_number = 5_000;
+let second_number = 10_000;
+
 function check_availability(income: number): boolean {
     if (income < 10_000) {
         return true;
@@ -13,10 +16,6 @@ console.log(check_availability(income))
 function add(first_number: number, second_number: number): number {
     return first_number + second_number;
 }
-
-let first_number = 5_000;
-let second_number = 10_000;
-
 console.log(add(first_number, second_number));
 
 //Anonymous Functions
@@ -25,3 +24,13 @@ let multipy = function(first_number: number, second_number: number): number {
 }
 
 console.log(multipy(first_number, second_number));
+
+//Arrow functions
+let first_name = "Hope";
+let last_name = "Sain";
+
+let full_name = (first_name: string, last_name: string): string => {
+    return first_name + " " + last_name;
+} 
+
+console.log(full_name(first_name, last_name));
