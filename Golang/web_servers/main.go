c3d46392c38d4d339462d0dc922e87985e17c636
hopesain <hopessain@gmail.com>
2026-02-01 16:36:55 +0200

    added routing.

--- Golang/web_servers/main.go
+++ Golang/web_servers/main.go
@@ -5,12 +5,29 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<h1>Hello, world</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "<h1>Not found, this page does not exist.</h1>")
+		return
+	}
+
+	fmt.Fprintln(w, "<h1>Home Page</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Contact Page</h1>")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>About Page</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about/", aboutHandler)
+	http.HandleFunc("/contact/", contactHandler)
+
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server running")
+
 }
