b2fe532dd1c567da6a4e738f0bba846df5ae7d18
hopesain <hopessain@gmail.com>
2026-02-04 15:19:48 +0200

    added the greetHandler.

--- Golang/web_api/main.go
+++ Golang/web_api/main.go
@@ -13,6 +13,8 @@ func main() {
 	slog.SetDefault(logger)
 
 	http.HandleFunc("/", helloworldHandler)
+	http.HandleFunc("/greet", greetHandler)
+	
 	fmt.Println("Hello world, it's Hope!")
 	http.ListenAndServe(":8080", nil)
 
@@ -27,5 +29,29 @@ func helloworldHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(map[string]string{"message": "Hello world, I am Hope!"})
+}
 
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "method not supported", http.StatusMethodNotAllowed)
+		return 
+	}
+
+	var body struct {
+		FullName string `json:"fullname"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil || body.FullName == "" {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return 
+	}
+
+	message := fmt.Sprintf("Hello %s! Nice to meet you.", body.FullName)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": message,
+	})
 }
