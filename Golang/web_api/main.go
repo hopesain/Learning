546d51265e07ae9a14aac92a0024ba3d957a9f12
hopesain <hopessain@gmail.com>
2026-02-04 15:49:53 +0200

    get books handler.

--- Golang/web_api/main.go
+++ Golang/web_api/main.go
@@ -1,3 +1,5 @@
+// Reference: https://opyjo.hashnode.dev/building-a-simple-rest-api-in-go
+
 package main
 
 import (
@@ -8,13 +10,22 @@ import (
 	"os"
 )
 
+func init() {
+	books = []Book{
+		{ID: "1", Title: "1984", Author: "George Orwell"},
+		{ID: "2", Title: "To Kill a Mockingbird", Author: "Harper Lee"},
+		{ID: "3", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald"},
+	}
+}
+
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
 	slog.SetDefault(logger)
 
 	http.HandleFunc("/", helloworldHandler)
 	http.HandleFunc("/greet", greetHandler)
-	
+	http.HandleFunc("/books", getBooks)
+
 	fmt.Println("Hello world, it's Hope!")
 	http.ListenAndServe(":8080", nil)
 
@@ -31,11 +42,10 @@ func helloworldHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "Hello world, I am Hope!"})
 }
 
-
 func greetHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "method not supported", http.StatusMethodNotAllowed)
-		return 
+		return
 	}
 
 	var body struct {
@@ -45,7 +55,7 @@ func greetHandler(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil || body.FullName == "" {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return 
+		return
 	}
 
 	message := fmt.Sprintf("Hello %s! Nice to meet you.", body.FullName)
@@ -55,3 +65,18 @@ func greetHandler(w http.ResponseWriter, r *http.Request) {
 		"message": message,
 	})
 }
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+//http://localhost:8080/book?id=1
