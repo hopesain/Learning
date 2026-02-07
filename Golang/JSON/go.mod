bd1afb5df8b468656110dc2777df5940a497bddd
hopesain <hopessain@gmail.com>
2026-02-07 19:16:08 +0200

    unmarshalling in golang.

---
+++ Golang/JSON/go.mod
@@ -0,0 +1,5 @@
+module learning/json
+
+go 1.25.3
+
+require github.com/sanity-io/litter v1.5.8 // indirect
--- Golang/JSON/main.go
+++ Golang/JSON/main.go
@@ -4,16 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"log/slog"
+	"os"
+
+	"github.com/sanity-io/litter"
 )
 
 func main() {
 	fmt.Println("Hello world!!")
 
 	newUser := User{
-		ID: 1,
+		ID:        1,
 		FirstName: "Hope",
-		LastName: "Sain",
-		Age: 24,
+		LastName:  "Sain",
+		Age:       24,
 	}
 
 	n, err := json.MarshalIndent(newUser, "", " ")
@@ -22,12 +25,25 @@ func main() {
 	}
 
 	fmt.Println(string(n))
+
+	var secondUser User
+
+	byteFile, err := os.ReadFile("data.json")
+	if err != nil {
+		slog.Error("Unable to read the file", "error", err)
+	}
+
+	err = json.Unmarshal(byteFile, &secondUser)
+	if err != nil {
+		slog.Error("unable to marshal the json", "error", err)
+	}
+
+	litter.Dump(secondUser)
 }
 
 type User struct {
-	ID int `json:"id"`
+	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
-	Age int `json:"age"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
 }
-
